64998e931e465576d9e0c9e3c71a5f6a24c37521
leandre postal <posl1401@usherbrooke.ca>
2024-12-27 16:01:06 -0500

    change language

--- my-website/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-      <header style={{ textAlign: "center", padding: "1rem", backgroundColor: "#f5f5f5" }}>
-        <h1>My Portfolio</h1>
-      </header>
-    );
-  }
